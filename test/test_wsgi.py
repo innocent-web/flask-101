cc572d3e513c4947c494d1165e9b52f1083ba51c
IANONA <ianona.innocent@gmail.com>
2022-03-16 03:21:26 +0000

    save modif

--- test/test_wsgi.py
+++ test/test_wsgi.py
@@ -12,3 +12,16 @@ class ApplicationTest(TestCase):
         body = reponse.data.decode()
         self.assertEqual(reponse.status_code, 200)
         self.assertTrue("Hello" in body)
+
+    def test_read_many_products(self):
+        response = self.client.get("/api/v1/products")
+        products = response.json
+        self.assertIsInstance(products, dict)
+        self.assertGreater(len(products), 1)  # 2 is not a mistake here.
+
+    def test_one_product(self):
+        rep = self.client.get("/api/v1/products/1")
+        produit = rep.json
+        self.assertEqual(rep.status_code, 200)
+        self.assertIsInstance(produit, dict)
+        self.assertEqual(produit['name'], 'Skello')
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return "Hello World les bado"
+
+
+@app.route('/api/v1/products')
+def product():
+    PRODUCTS = {
+        1: {'id': 1, 'name': 'Skello'},
+        2: {'id': 2, 'name': 'Socialive.tv'}
+    }
+    return PRODUCTS
+
+
+@app.route('/api/v1/products/<int:id>')
+def show_product():
+    one_product= {'id': request.args.id, 'name': request.args.name}
+    return jsonify(one_product)
